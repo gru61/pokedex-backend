1e5a38cab6eed3c3d2402f12c5c927f4a82f0b1d
Patrick <grubipatrick@gmail.com>
2025-06-14 19:00:48 +0200

    start neuaufbau des projekts

---
+++ src/main/java/repository/PokemonRepository.java
@@ -0,0 +1,8 @@
+package repository;
+
+import model.Pokemon;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PokemonRepository extends JpaRepository<Pokemon, Long> {
+
+}
